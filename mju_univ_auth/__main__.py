8bd99a7585296afd7d0d1b80508538f2dec98932
shinnk <sygys10293@gmail.com>
2025-12-17 13:30:39 +0900

    temp

--- mju_univ_auth/__main__.py
+++ mju_univ_auth/__main__.py
@@ -310,13 +310,13 @@ def main():
     
     # logging.getLogger().setLevel(logging.DEBUG)
     # # 테스트 실행
-    high_level_ok = test_high_level_api(user_id, user_pw)
-    service_results = test_all_services_login(user_id, user_pw)
-    fetcher_ok = test_fetchers_with_session(user_id, user_pw)
-    chaining_ok = test_chaining_api(user_id, user_pw)
+    # high_level_ok = test_high_level_api(user_id, user_pw)
+    # service_results = test_all_services_login(user_id, user_pw)
+    # fetcher_ok = test_fetchers_with_session(user_id, user_pw)
+    # chaining_ok = test_chaining_api(user_id, user_pw)
     
     # # 결과 요약
-    print_summary(high_level_ok, service_results, fetcher_ok, chaining_ok)
+    # print_summary(high_level_ok, service_results, fetcher_ok, chaining_ok)
     
     
     # 각 줄의 시간을 측정
@@ -332,9 +332,10 @@ def main():
     # end_time = time.time()
     # print(f"msi 세션 유효성 검사 시간: {end_time - start_time:.4f}초") # 평균 0.3초 내외
     
+    result = MjuUnivAuth(user_id=user_id, user_pw=user_pw, verbose=False).login("msi").get_student_basicinfo()
     # result = MjuUnivAuth(user_id=user_id, user_pw=user_pw, verbose=False).login("msi").get_student_card()
     # result = MjuUnivAuth(user_id=user_id, user_pw=user_pw, verbose=True).login("msi").get_student_changelog()
-    # print(result)
+    print(result)
     
     
 
